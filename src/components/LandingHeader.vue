6a3d4d8fd95516529324a06c803c60016c3f100f
Serikzhan <tlekkaliserik@icloud.com>
2024-11-06 09:37:09 +0500

    Landing Header desktop

--- src/components/LandingHeader.vue
+++ src/components/LandingHeader.vue
@@ -1,15 +1,115 @@
 <template>
-  <v-app-bar>
-    <!-- Header для Desktop -->
-    <div class="d-none d-md-flex">
-      <v-toolbar-title>Desktop Header</v-toolbar-title>
-      <!-- Другие элементы для Desktop -->
-    </div>
-
-    <!-- Header для Mobile -->
-    <div class="d-flex d-md-none">
-      <v-toolbar-title>Mobile Header</v-toolbar-title>
-      <!-- Другие элементы для Mobile -->
-    </div>
+  <v-app-bar height="80" elevation="0">
+    <v-container fluid>
+      <v-row>
+        <v-col class="logo" cols="4">
+          <v-toolbar-title>
+            <v-btn
+              to="/"
+              :ripple="false"
+              :active="false"
+              class="no-hover-opacity"
+            >
+              <span class="title-ai">AI</span>
+              <span class="title-nudes">Nudes</span>
+            </v-btn>
+          </v-toolbar-title>
+        </v-col>
+        <v-col cols="4" class="d-flex align-center justify-center">
+          <v-btn>
+            <span class="anchor-tag">Possibilities</span>
+          </v-btn>
+          <v-btn> <span class="anchor-tag">Instructions</span></v-btn>
+          <v-btn> <span class="anchor-tag">Pricing</span></v-btn>
+        </v-col>
+        <v-col cols="4" class="text-right pr-4">
+          <v-btn
+            class="mr-2"
+            icon="mdi-theme-light-dark"
+            @click="toggleTheme"
+          ></v-btn>
+          <v-btn
+            min-height="40"
+            min-width="150"
+            rounded="xl"
+            size="x-large"
+            to="/"
+            class="login-btn"
+          >
+            <h4 class="login">Login/Register</h4>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
+
+<script setup>
+import { useTheme } from "vuetify";
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
+
+<style scoped>
+.title-ai {
+  color: #ffbaf3;
+  font-size: 24px; /* Цвет для слова AI */
+  letter-spacing: -0.7px;
+}
+
+.title-nudes {
+  color: var(--v-theme-on-background);
+  transition: color 0.9s ease;
+  margin-left: 3px;
+  font-size: 24px;
+  letter-spacing: -0.7px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  padding-left: 35px;
+}
+
+.no-hover-opacity {
+  --v-hover-opacity: 0 !important;
+  padding-left: 0px;
+}
+
+.anchor-tag {
+  color: var(--v-theme-on-background);
+  opacity: 0.6;
+  transition: color 0.3s;
+  font-size: 18px;
+  letter-spacing: 0px;
+}
+
+.v-theme--light .login-btn {
+  background-color: black;
+}
+
+.v-theme--light .login {
+  color: white;
+
+  transition: color 0.3s;
+  font-size: 18px;
+  letter-spacing: 0px;
+  font-weight: 700;
+}
+
+.v-theme--dark .login-btn {
+  background-color: #333333;
+}
+
+.v-theme--dark .login {
+  color: white;
+
+  transition: color 0.3s;
+  font-size: 18px;
+  letter-spacing: 0px;
+  font-weight: 700;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,20 +11,11 @@ import '../styles/main.css'
 // Composables
 import { createVuetify } from 'vuetify'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+
 export default createVuetify({
   theme: {
-    defaultTheme: 'myCustomTheme',
-    themes: {
-      myCustomTheme: {
-          colors: {
-              primary: '#2196F3', // Задайте свои цвета, если нужно
-              // другие цвета...
-          },
-          variables: {
-              fontFamily: 'Onest, sans-serif', // Замените на свой шрифт
-          },
-      },
-  },
+    defaultTheme: 'light',
+    
   },
 })
